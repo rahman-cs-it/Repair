f8dbc38b777fd4e21d121c40aa791a26047f3465
Rahman it <siddiquewasifur786123@gmail.com>
2026-06-10 22:19:59 +0530

    added speed-insights from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { useState } from 'react'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
+
 
 
 function App() {
